0d2d5793de02e30cdf83d79839d8c4c7b6a8837e
Viktor <viktor@intrykk.se>
2020-04-03 02:53:07 +0200

    Use absolute dir path

--- scripts/deploy_az_storage.sh
+++ scripts/deploy_az_storage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Running deploy_az_storage.sh..."
-az storage blob upload-batch -d \$web -s ../out
+az storage blob upload-batch -d \$web -s $TRAVIS_BUILD_DIR/out
 echo "Script deploy_az_storage done."
