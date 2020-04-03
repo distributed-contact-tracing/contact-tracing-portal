9072b92ccaf28be4fcddb0c59ce6d74036099b63
Viktor <viktor@intrykk.se>
2020-04-03 11:01:30 +0200

    Remove header text

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,5 @@
 export const Header = () => (
   <header>
-    <h1>DISCOT</h1>
-
-    <style jsx>{`
-      h1 {
-        font-size: 1.5rem;
-        text-align: center;
-      }
-    `}</style>
+    <style jsx>{``}</style>
   </header>
 );
