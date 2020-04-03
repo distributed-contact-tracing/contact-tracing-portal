8ab5c209ce7269e0145f04090ee33dbfa166ea72
Viktor <viktor@intrykk.se>
2020-04-03 03:31:48 +0200

    Debug build script

--- scripts/deploy_az_storage.sh
+++ scripts/deploy_az_storage.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 echo "Running deploy_az_storage.sh..."
+echo $TRAVIS_BUILD_DIR
+cd $TRAVIS_BUILD_DIR
+ls
 az storage blob upload-batch -d \$web -s $TRAVIS_BUILD_DIR/out
 echo "Script deploy_az_storage done."
