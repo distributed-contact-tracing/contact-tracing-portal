954831c20c432cd84e1cfdec745dd18db9aceb1b
Viktor <viktor@intrykk.se>
2020-04-03 02:38:33 +0200

    Fix relative path

--- scripts/deploy_az_storage.sh
+++ scripts/deploy_az_storage.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-az storage blob upload-batch -d \$web -s ./out
+echo "Running deploy_az_storage.sh..."
+az storage blob upload-batch -d \$web -s ../out
+echo "Script deploy_az_storage done."
